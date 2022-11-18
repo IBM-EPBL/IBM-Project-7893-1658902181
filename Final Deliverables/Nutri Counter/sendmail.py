f40dbee94cc6c835a2ae079b35ef09a3156bb9da
harikrishnan0777 <114554120+harikrishnan0777@users.noreply.github.com>
2022-11-18 22:39:50 +0530

    Add files via upload

---
+++ Final Deliverables/Nutri Counter/sendmail.py
@@ -0,0 +1,19 @@
+from sendgrid import SendGridAPIClient
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+sg = SendGridAPIClient('SENDGRID_API_KEY')
+from_email = Email("nutricounter.cc@gmail.com")  # Change to your verified sender
+to_email = To("nutricounter.cc@gmail.com")  # Change to your recipient
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+
+# Get a JSON-ready representation of the Mail object
+mail_json = mail.get()
+
+# Send an HTTP POST request to /mail/send
+response = sg.client.mail.send.post(request_body=mail_json)
+print(response.status_code)
+print(response.headers)
+Footer
